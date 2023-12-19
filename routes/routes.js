78d37f281677434128e3b6b0d691f20d7473ca02
francisco <63866254+ka33al@users.noreply.github.com>
2023-12-19 18:39:54 +0000

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 const routes = require("express").Router()
     const TaskController = require("../controller/TaskController")
 
-routes.get("/",TaskController.getallTasks  )
-routes.post("/create", TaskController.createTask)
-routes.get("/getById/:id/:method",TaskController.getById)
-routes.post("/updateOne/:id",TaskController.updateOneTask)
-routes.get("/deleteOne/:id",TaskController.deleteOneTask)
+routes.get("https://ka33al.github.io/cadastroclientes/",TaskController.getallTasks  )
+routes.post("https://ka33al.github.io/cadastroclientes/create", TaskController.createTask)
+routes.get("https://ka33al.github.io/cadastroclientes/getById/:id/:method",TaskController.getById)
+routes.post("https://ka33al.github.io/cadastroclientes/updateOne/:id",TaskController.updateOneTask)
+routes.get("https://ka33al.github.io/cadastroclientes/deleteOne/:id",TaskController.deleteOneTask)
 
 
 module.exports= routes
